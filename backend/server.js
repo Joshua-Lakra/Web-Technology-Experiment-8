4748412ed1901237552df8019be5629127f4cf4f
Joshua-Lakra <joshualakra5@gmail.com>
2025-10-26 17:28:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const http = require("http");
 const { connectDB } = require("./db");
 const { insertEmployee, fetchEmployees, deleteEmployee } = require("./employee");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 connectDB().then(() => {
   const server = http.createServer(async (req, res) => {
